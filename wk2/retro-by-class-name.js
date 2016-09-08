9bb84aaae0d5bf8c1122b7eaff634cdff5a16408
Karen Nemeth <karen61990@gmail.com>
2016-09-08 13:24:27 +0200

    added multi-class-name edge case

--- wk2/retro-by-class-name.js
+++ wk2/retro-by-class-name.js
@@ -3,19 +3,30 @@
         var startingPoint = document.body;
         var desiredElements = [];
 
-        function letsDoThis(something, theArray) {
+        function letsDoThis(something) {
             var childrenOf = something.children;
             for (var i=0; i < childrenOf.length; i++) {
-                if (childrenOf[i].className === findThisClass) {
-                    theArray.push(childrenOf[i]);
+                if (childrenOf[i].className === findThisClass {
+                    desiredElements.push(childrenOf[i]);
                 }
-                letsDoThis(childrenOf[i], desiredElements);
+                else {
+                    var splitClasses = childrenOf[i].className.split(" ");
+                    for (var j=0; j < splitClasses.length; j++) {
+                        if (splitClasses[j].className === findThisClass) {
+                            desiredElements.push(splitClasses[j]);
+                        }
+                    }
+                }
+                // to check in thecondition of "catdog", etc. use toCharArray to split the classname up, then make the desired classname into an array and wait for the characters to match. if they do record if not move on.
+
+                letsDoThis(childrenOf[i]);
             }
         }
 
         letsDoThis(startingPoint, desiredElements);
 
         console.log(desiredElements);
+        return(desiredElements);
     }
 
     getClasses("cat");
