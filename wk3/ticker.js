581139e7e89f0062099e707f844d7cde3b4b983a
Karen Nemeth <karen61990@gmail.com>
2016-09-13 11:19:31 +0200

    working with x=88

--- wk3/ticker.js
+++ wk3/ticker.js
@@ -1,3 +1,28 @@
+
 (function() {
+
+    var ticker = document.getElementsByClassName('container');
+    var tickerChildren = ticker[0].children;
+    var links = document.getElementsByClassName('link');
+    var x = 88;
+
+
+    function roundAbout() {
+
+        for (var i = 0; i < links.length; i++) {
+            links[i].style.left = x+"%";
+            x -= 0.05;
+        }
+        if (x<-30){
+            window.cancelAnimationFrame(roundAbout);
+            ticker[0].appendChild(tickerChildren[0]);
+            x = 88;
+            console.log("in the loop");
+        }
+        window.requestAnimationFrame(roundAbout);
+    }
+
+    roundAbout();
+
 
 })();
