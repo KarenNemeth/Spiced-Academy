dea1d53a317062264c382784102ff44eb6811b25
Karen Nemeth <karen61990@gmail.com>
2016-09-13 14:37:50 +0200

    complete with movement and mouseover

--- wk3/ticker.js
+++ wk3/ticker.js
@@ -3,26 +3,41 @@
 
     var ticker = document.getElementsByClassName('container');
     var tickerChildren = ticker[0].children;
-    var links = document.getElementsByClassName('link');
-    var x = 88;
+    var x = 100;
+    var width;
+    var noEvent = true;
 
 
     function roundAbout() {
 
-        for (var i = 0; i < links.length; i++) {
-            links[i].style.left = x+"%";
-            x -= 0.05;
-        }
-        if (x<-30){
+        ticker[0].style.left = x+"%";
+        x -= 0.17;
+
+        width = (Math.round(((tickerChildren[0].offsetWidth/document.querySelectorAll('body')[0].clientWidth)-0)*100));
+
+        if (x < -width){
             window.cancelAnimationFrame(roundAbout);
+            x += (width+0.5);
             ticker[0].appendChild(tickerChildren[0]);
-            x = 88;
-            console.log("in the loop");
+            ticker[0].style.left = x+"%";
         }
-        window.requestAnimationFrame(roundAbout);
+
+        if (noEvent) {
+            window.requestAnimationFrame(roundAbout);
+        }
     }
 
     roundAbout();
 
+    ticker[0].addEventListener("mouseenter", function(){
+        noEvent = false;
+        window.cancelAnimationFrame(roundAbout);
+        console.log("hovered!");
+    });
+    ticker[0].addEventListener("mouseleave", function(){
+        noEvent = true;
+        roundAbout();
+        console.log("left");
+    })
 
 })();
