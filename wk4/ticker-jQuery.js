f5363b3fe5f2a99fb257dbede4d88b189eea2a90
Karen Nemeth <karen61990@gmail.com>
2016-09-21 12:20:31 +0200

    ticker with Handlebars

--- wk4/ticker-jQuery.js
+++ wk4/ticker-jQuery.js
@@ -1,13 +1,19 @@
-
 $(document).ready(function() {
+    var templates = document.querySelectorAll('script[type="text/handlebars"]');
+
+    Handlebars.templates = Handlebars.templates || {};
+    Handlebars.partials = Handlebars.templates;
+
+    Array.prototype.slice.call(templates).forEach(function(script) {
+        Handlebars.templates[script.id] = Handlebars.compile(script.innerHTML);
+    });
 
     $.getJSON('ticker-JSON-links.json', function(links) {
-        console.log(links);
-        $.each(links, function(key, val){
-            console.log(val.title);
-            console.log(val.link);
-            $('<a href="' + val.link + '">' + val.title +'</a>').addClass("link").appendTo('.container');
+        var template = Handlebars.templates.itemLinks({
+            links: links
         });
+        $(".container").html(template);
+        console.log(template);
     });
 
     var x = 100;
